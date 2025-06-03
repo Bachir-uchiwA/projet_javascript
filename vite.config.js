a9199587c3da8022ce50847c12ae6c09104888db
Uchiwa75567 <pabassdiame76@gmail.com>
2025-06-03 15:02:55 +0000

    reglace4

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+export default {
+  server: {
+    port: 5173, // ou le port que tu veux
+    open: true, // ouvre le navigateur automatiquement
+  },
+  publicDir: 'public', // par défaut, inutile de le préciser si tu utilises 'public'
+}
